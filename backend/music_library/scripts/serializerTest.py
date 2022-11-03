59dc1d04bd3444a84230821116238054b1e544d2
JanpaulParedero <janpaulparedero@gmail.com>
2022-11-03 00:41:39 -0400

    endpoints and user auth

--- backend/music_library/scripts/serializerTest.py
+++
@@ -1,7 +0,0 @@
-from api.models import Album
-from api.serializers import AlbumSerializer
-
-def run():
-    test_obj = Album.objects.get(pk='spotify:album:0ETFjACtuP2ADo6LFhL6HN')
-    test_serializer = AlbumSerializer(instance=test_obj)
-    print(test_serializer.data)
