8ff83af71cde195e023e974ba7160b38aa9e6565
JanpaulParedero <janpaulparedero@gmail.com>
2022-11-16 22:53:39 -0500

    Added login

--- music_library_client/src/App.js
+++ music_library_client/src/App.js
@@ -6,21 +6,30 @@ import { Container } from "semantic-ui-react"
 import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AlbumPage from './components/AlbumPage';
+import Register from './components/Register';
+import Login from './components/Login';
+import { AuthProvider } from './utils/AuthContext';
+//import PrivateRoute from './utils/PrivateRoute';
+
 
 const AppContainer = styled(Container)`
-  margin: 15px;
+  margin: 15px; 
 `
 const App = () => {
 
   return (
     <Router>
-      <AppContainer>
-        <Navbar />
-          <Routes>
-            <Route path="/" element={<AlbumLists/>} />
-            <Route path="/:albumURI" element={<AlbumPage/>} />
-          </Routes>
-      </AppContainer>
+      <AuthProvider>
+        <AppContainer>
+          <Navbar />
+            <Routes>
+              <Route path="/" element={<AlbumLists/>} />
+              <Route path="/:albumURI" element={<AlbumPage/>} />
+              <Route path="/register" element={<Register/>}/>
+              <Route path="/login" element={<Login/>}/>
+            </Routes>
+        </AppContainer>
+      </AuthProvider>
     </Router>
   );
 }
---
+++ music_library_client/src/utils/AuthContext.js
@@ -0,0 +1,57 @@
+import { createContext, useState} from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext()
+
+export default AuthContext;
+
+export const AuthProvider = ({children}) => {
+    
+    let [authTokens, setAuthTokens] = useState(() => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
+    let [user, setUser] = useState(() => localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null)
+    let navigate = useNavigate();
+    
+
+    let loginUser = async (e )=> { 
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/token/', 
+                {username: e.target.username.value , password : e.target.password.value});
+            console.log(response.data)
+            console.log (response.data.access)
+            if(response.status === 200){
+                setAuthTokens(response.data)
+                setUser(jwt_decode(response.data.access))
+                localStorage.setItem('authTokens', JSON.stringify(response.data))
+                navigate("/");
+            }
+            else{
+                alert("Something ain't right!")
+            }
+        }
+        catch (err) {
+         console.log(err);
+        }
+    }
+
+    let logoutUser = () => {
+        setAuthTokens(null)
+        setUser(null)
+        localStorage.removeItem('authTokens')
+        navigate("/");
+    }
+
+    let contextData = {
+        user:user,
+        loginUser:loginUser,
+        logoutUser:logoutUser
+    }
+    return(
+        <AuthContext.Provider value={contextData}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
